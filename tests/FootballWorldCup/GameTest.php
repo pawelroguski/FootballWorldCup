7dadacf127c11f2d4656b414908384a2d6684a33
Paweł Roguski <pawel.roguski@intive.com>
2023-09-29 21:19:16 +0200

    Add boilerplate with Game Class test cases

---
+++ tests/FootballWorldCup/GameTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace FootballWorldCup\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class GameTest extends TestCase
+{
+    public function test_should_pass_when_both_teams_scores_updated_properly(){}
+
+    public function test_should_pass_when_teams_scores_add_properly(){}
+
+    public function test_should_pass_when_teams_names_returns_properly(){}
+}
