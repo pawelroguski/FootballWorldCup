6606692e8fe93afd2d59082b7b3712c4297a412f
Paweł Roguski <pawel.roguski@intive.com>
2023-09-29 21:22:43 +0200

    Add boilerplate with ScoreBoard Class test cases

---
+++ tests/FootballWorldCup/ScoreBoardTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace FootballWorldCup\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ScoreBoardTest extends TestCase
+{
+    public function test_should_return_null_in_case_of_pass_empty_array_parameter(){}
+
+    public function test_should_throw_error_in_case_of_pass_non_array_parameter(){}
+
+    public function test_should_pass_when_result_is_presented_in_correct_order(){}
+}
