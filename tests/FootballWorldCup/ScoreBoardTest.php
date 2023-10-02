72915f80d630f56a1da3e7949005906153604f4f
Paweł Roguski <pawel.roguski@intive.com>
2023-10-02 14:17:31 +0200

    Add ScoreBoard test cases

--- tests/FootballWorldCup/ScoreBoardTest.php
+++ tests/FootballWorldCup/ScoreBoardTest.php
@@ -6,9 +6,29 @@ use PHPUnit\Framework\TestCase;
 
 class ScoreBoardTest extends TestCase
 {
-    public function test_should_return_null_in_case_of_pass_empty_array_parameter(){}
+    public function test_should_return_null_in_case_of_pass_empty_array_parameter(){
+        $scoreBoard = new ScoreBoard();
+        $summary = $scoreBoard->getSummary([]);
+        $this->assertEmpty($summary);
+    }
 
-    public function test_should_throw_error_in_case_of_pass_non_array_parameter(){}
+    public function test_should_throw_error_in_case_of_pass_non_array_parameter(){
+        $scoreBoard = new ScoreBoard();
+        $this->expectException(TypeError::class);
+        $summary = $scoreBoard->getSummary(null);
+    }
 
-    public function test_should_pass_when_result_is_presented_in_correct_order(){}
+    public function test_should_pass_when_result_is_presented_in_correct_order(){
+        $manager = new GameManager();
+        $scoreBoard = new ScoreBoard();
+        $game1 = new Game('TeamA', 'TeamB');
+        $game2 = new Game('TeamC', 'TeamD');
+        $manager->startGame($game1);
+        $manager->startGame($game2);
+        $manager->updateScore($game1,2, 3);
+        $manager->updateScore($game2,3, 2);
+        $summary = $scoreBoard->getSummary([$game1, $game2]);
+        $expected = "TeamC 3 - TeamD 2\nTeamA 2 - TeamB 3\n";
+        $this->assertEquals($expected, $summary);
+    }
 }
