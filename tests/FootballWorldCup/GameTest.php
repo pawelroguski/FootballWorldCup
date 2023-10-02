35bb2f52cb6bde93caa04b788df7456c51403739
Paweł Roguski <pawel.roguski@intive.com>
2023-10-02 14:15:30 +0200

    Add Game test cases

--- tests/FootballWorldCup/GameTest.php
+++ tests/FootballWorldCup/GameTest.php
@@ -6,9 +6,24 @@ use PHPUnit\Framework\TestCase;
 
 class GameTest extends TestCase
 {
-    public function test_should_pass_when_both_teams_scores_updated_properly(){}
+    public function test_should_pass_when_both_teams_scores_updated_properly(){
+        $game = new Game('TeamA', 'TeamB');
+        $game->setHomeTeamScore(2);
+        $game->setAwayTeamScore(3);
+        $this->assertEquals(2, $game->getHomeTeamScore());
+        $this->assertEquals(3, $game->getAwayTeamScore());
+    }
 
-    public function test_should_pass_when_teams_scores_add_properly(){}
+    public function test_should_pass_when_teams_scores_add_properly(){
+        $game = new Game('TeamA', 'TeamB');
+        $game->setHomeTeamScore(2);
+        $game->setAwayTeamScore(3);
+        $this->assertEquals(5, $game->getTotalScore());
+    }
 
-    public function test_should_pass_when_teams_names_returns_properly(){}
+    public function test_should_pass_when_teams_names_returns_properly(){
+        $game = new Game('TeamA', 'TeamB');
+        $this->assertEquals('TeamA', $game->getHomeTeamName());
+        $this->assertEquals('TeamB', $game->getAwayTeamName());
+    }
 }
