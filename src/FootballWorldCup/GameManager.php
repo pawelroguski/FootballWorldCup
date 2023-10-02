b467fa139bb6c7cbe4c6b5e030e63eb2cd68d2b1
Paweł Roguski <pawel.roguski@intive.com>
2023-10-02 15:36:20 +0200

    Refactor GameManager class and GameManagerInterface
    - add Doc comments
    - add annotations

--- src/FootballWorldCup/GameManager.php
+++ src/FootballWorldCup/GameManager.php
@@ -1,14 +1,34 @@
 <?php
-
 namespace FootballWorldCup;
 
 use InvalidArgumentException;
 use LogicException;
 
+/**
+ * GameManager Class
+ *
+ * @category Class
+ * @package  FootballWorldCup
+ * @author   Pawel Roguski <kontakt@pawelroguski.pl>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     www.pawelroguski.pl
+ */
 class GameManager implements GameManagerInterface
 {
+    /**
+     * All currently started games
+     *
+     * @var array
+     */
     protected array $allGames = [];
 
+    /**
+     * Start game action
+     *
+     * @param GameInterface $game Game object
+     *
+     * @return void
+     */
     public function startGame(GameInterface $game): void
     {
         if ($this->_returnGameIndexOrFalse($game) !== false) {
@@ -18,6 +38,13 @@ class GameManager implements GameManagerInterface
         $this->allGames[] = $game;
     }
 
+    /**
+     * Finish game action
+     *
+     * @param GameInterface $game Game object
+     *
+     * @return void
+     */
     public function finishGame(GameInterface $game): void
     {
         $index = $this->_returnGameIndexOrFalse($game);
@@ -27,12 +54,20 @@ class GameManager implements GameManagerInterface
         unset($this->allGames[$index]);
     }
 
+    /**
+     * Updates Home and Away teams scores values
+     *
+     * @param GameInterface $game          Game object
+     * @param int           $homeTeamScore Home team score value
+     * @param int           $awayTeamScore Away team score value
+     *
+     * @return void
+     */
     public function updateScore(
         GameInterface $game,
         int $homeTeamScore,
         int $awayTeamScore
-    ): void
-    {
+    ): void {
         if ($homeTeamScore < 0 || $awayTeamScore < 0) {
             throw new InvalidArgumentException();
         }
@@ -43,13 +78,26 @@ class GameManager implements GameManagerInterface
         $game->setAwayTeamScore($awayTeamScore);
     }
 
+    /**
+     * Returns all currently started games
+     *
+     * @return array
+     */
     public function getAllGames(): array
     {
         return $this->allGames;
     }
 
+    /**
+     * Returns index of found game or false in case if not found
+     *
+     * @param GameInterface $game Game object
+     *
+     * @return false|int
+     */
     private function _returnGameIndexOrFalse(GameInterface $game): false|int
     {
         return array_search($game, $this->getAllGames());
     }
 }
+
--- src/FootballWorldCup/GameManagerInterface.php
+++ src/FootballWorldCup/GameManagerInterface.php
@@ -2,14 +2,54 @@
 
 namespace FootballWorldCup;
 
+/**
+ * GameManager Interface
+ *
+ * @category Interface
+ * @package  FootballWorldCup
+ * @author   Pawel Roguski <kontakt@pawelroguski.pl>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     www.pawelroguski.pl
+ */
 interface GameManagerInterface
 {
+    /**
+     * Start game action
+     *
+     * @param GameInterface $game Game object
+     *
+     * @return void
+     */
     public function startGame(GameInterface $game): void;
+
+    /**
+     * Finish game action
+     *
+     * @param GameInterface $game Game object
+     *
+     * @return void
+     */
     public function finishGame(GameInterface $game): void;
+
+    /**
+     * Updates Home and Away teams scores values
+     *
+     * @param GameInterface $game          Game object
+     * @param int           $homeTeamScore Home team score value
+     * @param int           $awayTeamScore Away team score value
+     *
+     * @return void
+     */
     public function updateScore(
         GameInterface $game,
         int $homeTeamScore,
         int $awayTeamScore
     ): void;
+
+    /**
+     * Returns all currently started games
+     *
+     * @return array
+     */
     public function getAllGames(): array;
 }
