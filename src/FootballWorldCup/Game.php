9d2b05ad091634f2655c135129db9a026faa78aa
Paweł Roguski <pawel.roguski@intive.com>
2023-10-02 14:53:18 +0200

    Game class and Game interface refactor
    - add Doc comments
    - add annotations

--- src/FootballWorldCup/Game.php
+++ src/FootballWorldCup/Game.php
@@ -1,14 +1,43 @@
 <?php
+declare(strict_types=1);
 
 namespace FootballWorldCup;
 
 use InvalidArgumentException;
 
-class Game
+/**
+ * Game Class
+ *
+ * @category Class
+ * @package  FootballWorldCup
+ * @author   Pawel Roguski <kontakt@pawelroguski.pl>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     www.pawelroguski.pl
+ */
+class Game implements GameInterface
 {
+    /**
+     * Home team score value
+     *
+     * @var int
+     */
     private int $_homeTeamScore = 0;
+
+    /**
+     * Away team score value
+     *
+     * @var int
+     */
     private int $_awayTeamScore = 0;
 
+    /**
+     * Game Class constructor
+     *
+     * @param string $_homeTeamName Home team name
+     * @param string $_awayTeamName Away team name
+     *
+     * @throws InvalidArgumentException When one of the arguments is empty
+     */
     public function __construct(
         private readonly string $_homeTeamName,
         private readonly string $_awayTeamName
@@ -18,36 +47,75 @@ class Game
         }
     }
 
+    /**
+     * Sets the value of Home team score
+     *
+     * @param int $homeTeamScore Home team score value
+     *
+     * @return void
+     */
     public function setHomeTeamScore(int $homeTeamScore): void
     {
         $this->_homeTeamScore = $homeTeamScore;
     }
 
+    /**
+     * Sets the value of Away team score
+     *
+     * @param int $awayTeamScore Away team score value
+     *
+     * @return void
+     */
     public function setAwayTeamScore(int $awayTeamScore): void
     {
         $this->_awayTeamScore = $awayTeamScore;
     }
 
+    /**
+     * Get Home team name
+     *
+     * @return string
+     */
     public function getHomeTeamName(): string
     {
         return $this->_homeTeamName;
     }
 
+    /**
+     * Get Away team name
+     *
+     * @return string
+     */
     public function getAwayTeamName(): string
     {
         return $this->_awayTeamName;
     }
 
+    /**
+     * Get score of Home team
+     *
+     * @return int
+     */
     public function getHomeTeamScore(): int
     {
         return $this->_homeTeamScore;
     }
 
+    /**
+     * Get score of Away Team
+     *
+     * @return int
+     */
     public function getAwayTeamScore(): int
     {
         return $this->_awayTeamScore;
     }
 
+    /**
+     * Get score summary of Home and Away Teams
+     *
+     * @return int
+     */
     public function getTotalScore(): int
     {
         return $this->_homeTeamScore + $this->_awayTeamScore;
--- src/FootballWorldCup/GameInterface.php
+++ src/FootballWorldCup/GameInterface.php
@@ -2,13 +2,67 @@
 
 namespace FootballWorldCup;
 
+/**
+ * Game Interface
+ *
+ * @category Interface
+ * @package  FootballWorldCup
+ * @author   Pawel Roguski <kontakt@pawelroguski.pl>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     www.pawelroguski.pl
+ */
 interface GameInterface
 {
+    /**
+     * Get Home team name
+     *
+     * @return string
+     */
     public function getHomeTeamName(): string;
+
+    /**
+     * Get Away team name
+     *
+     * @return string
+     */
     public function getAwayTeamName(): string;
+
+    /**
+     * Sets the value of Home team score
+     *
+     * @param int $homeTeamScore Home team score value
+     *
+     * @return void
+     */
     public function setHomeTeamScore(int $homeTeamScore): void;
+
+    /**
+     * Get Home team score value
+     *
+     * @return int
+     */
     public function getHomeTeamScore(): int;
+
+    /**
+     * Sets the value of Away team score
+     *
+     * @param int $awayTeamScore Away team score value
+     *
+     * @return void
+     */
     public function setAwayTeamScore(int $awayTeamScore): void;
+
+    /**
+     * Get Away team score value
+     *
+     * @return int
+     */
     public function getAwayTeamScore(): int;
+
+    /**
+     * Get Home and Away teams scores values
+     *
+     * @return int
+     */
     public function getTotalScore(): int;
 }
