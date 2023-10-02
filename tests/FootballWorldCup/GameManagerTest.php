7bfb12446c76fbd5f49b38523dc13e4c06f73483
Paweł Roguski <pawel.roguski@intive.com>
2023-10-02 13:40:19 +0200

    Add GameManager test cases

--- tests/FootballWorldCup/GameManagerTest.php
+++ tests/FootballWorldCup/GameManagerTest.php
@@ -2,25 +2,147 @@
 
 namespace FootballWorldCup\Tests;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
+use TypeError;
 
 class GameManagerTest extends TestCase
 {
-    public function test_should_passed_when_game_launched_and_completed_correctly(){}
+    public function test_should_passed_when_game_launched_and_completed_correctly(){
+        $manager = new GameManager();
+        $game = new Game('TeamA', 'TeamB');
+        $manager->startGame($game);
+        $this->assertCount(1, $manager->getAllGames());
+        $manager->finishGame($game);
+        $this->assertCount(0, $manager->getAllGames());
+    }
 
-    public function test_should_pass_when_both_teams_scores_updated_properly(){}
+    public function test_should_pass_when_both_teams_scores_updated_properly(){
+        $manager = new GameManager();
+        $game = new Game('TeamA', 'TeamB');
+        $manager->startGame($game);
+        $manager->updateScore($game, 2, 3);
+        $this->assertEquals(2, $game->getHomeTeamScore());
+        $this->assertEquals(3, $game->getAwayTeamScore());
+    }
 
-    public function test_should_throw_error_when_negative_number_passed_into_score(){}
+    public function test_should_throw_error_when_negative_number_passed_into_score(
+        int $homeTeamScore,
+        int $awayTeamScore,
+        string $expectedException
+    ){
+        $manager = new GameManager();
+        $game = new Game('TeamA', 'TeamB');
+        $manager->startGame($game);
+        $this->expectException($expectedException);
+        $manager->updateScore($game, $homeTeamScore, $awayTeamScore);
+    }
 
-    public function test_should_throw_error_after_attempt_create_game_with_null_or_empty_team_value(){}
+    public function test_should_throw_error_after_attempt_create_game_with_null_or_empty_team_value(
+        ?string $homeTeamName,
+        ?string $awayTeamName,
+        string $expectedException
+    ){
+        $manager = new GameManager();
+        $this->expectException($expectedException);
+        $game = new Game($homeTeamName, $awayTeamName);
+        $manager->startGame($game);
+    }
 
-    public function test_should_throw_error_after_attempt_finish_not_started_game(){}
+    public function test_should_throw_error_after_attempt_finish_not_started_game(){
+        $manager = new GameManager();
+        $game = new Game('TeamA', 'TeamB');
+        $this->expectException(LogicException::class);
+        $manager->finishGame($game);
+    }
 
-    public function test_should_throw_error_after_attempt_start_same_game_multiple_times(){}
+    public function test_should_throw_error_after_attempt_start_same_game_multiple_times(){
+        $manager = new GameManager();
+        $game = new Game('TeamA', 'TeamB');
+        $manager->startGame($game);
+        $this->expectException(LogicException::class);
+        $manager->startGame($game);
+    }
 
-    public function test_should_throw_error_after_attempt_finish_same_game_multiple_times(){}
+    public function test_should_throw_error_after_attempt_finish_same_game_multiple_times(){
+        $manager = new GameManager();
+        $game = new Game('TeamA', 'TeamB');
+        $manager->startGame($game);
+        $manager->finishGame($game);
+        $this->expectException(LogicException::class);
+        $manager->finishGame($game);
+    }
 
-    public function test_should_throw_error_after_attempt_update_score_for_finished_game(){}
+    public function test_should_throw_error_after_attempt_update_score_for_finished_game(){
+        $manager = new GameManager();
+        $game = new Game('TeamA', 'TeamB');
+        $manager->startGame($game);
+        $manager->finishGame($game);
+        $this->expectException(LogicException::class);
+        $manager->updateScore($game, 1, 2);
+    }
 
-    public function test_should_throw_error_after_attempt_update_score_for_not_started_game(){}
+    public function test_should_throw_error_after_attempt_update_score_for_not_started_game(){
+        $manager = new GameManager();
+        $game = new Game('TeamA', 'TeamB');
+        $this->expectException(LogicException::class);
+        $manager->updateScore($game, 1, 2);
+    }
+
+    public static function provideNullValuesForTeamNames(): array
+    {
+        return [
+            'null home team name' => [
+                'homeTeamName' => null,
+                'awayTeamName' => 'TeamB',
+                'expectedException' => TypeError::class
+            ],
+            'null away team name' => [
+                'homeTeamName' => 'TeamA',
+                'awayTeamName' => null,
+                'expectedException' => TypeError::class
+            ],
+            'null both team names' => [
+                'homeTeamName' => null,
+                'awayTeamName' => null,
+                'expectedException' => TypeError::class
+            ],
+            'empty home team name' => [
+                'homeTeamName' => "",
+                'awayTeamName' => 'TeamB',
+                'expectedException' => InvalidArgumentException::class
+            ],
+            'empty away team name' => [
+                'homeTeamName' => 'TeamA',
+                'awayTeamName' => "",
+                'expectedException' => InvalidArgumentException::class
+            ],
+            'empty both team names' => [
+                'homeTeamName' => "",
+                'awayTeamName' => "",
+                'expectedException' => InvalidArgumentException::class
+            ],
+        ];
+    }
+
+    public static function provideNegativeValuesForScore(): array
+    {
+        return [
+            'negative score value for home team' => [
+                'homeTeamScore' => -1,
+                'awayTeamScore' => 2,
+                'expectedException' => InvalidArgumentException::class
+            ],
+            'negative score value for away team' => [
+                'homeTeamScore' => 1,
+                'awayTeamScore' => -2,
+                'expectedException' => InvalidArgumentException::class
+            ],
+            'negative score value for both teams' => [
+                'homeTeamScore' => -1,
+                'awayTeamScore' => -2,
+                'expectedException' => InvalidArgumentException::class
+            ],
+        ];
+    }
 }
